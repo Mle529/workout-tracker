1bd4a9649b26ac9025d9748b42a522c00623db7c
Michael Le <mnle529@gmail.com>
2020-03-22 15:40:02 -0700

    updated server with dotenv

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout-tracker-bc";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout-tracker-ucibc";
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useFindAndModify: false
